ad7cc958207e9560cacbe0b88a641683d8530e28
Bn4 <bnfour@bnfour.net>
2024-01-13 16:53:45 +0700

    file replacement actually implemented

--- proto-godot-repacker.py
+++ proto-godot-repacker.py
@@ -2,6 +2,7 @@
 
 import argparse
 import io
+import hashlib
 import mmap
 import os
 import struct
@@ -44,6 +45,8 @@ def dir_type(path: str) -> str:
 
 #endregion
 
+# region file manipulating
+
 def is_godot_resource(f: mmap.mmap) -> bool:
     magic = b"GDPC"
     if f.read(4) == magic:
@@ -67,12 +70,30 @@ def try_to_find_resource(f: mmap.mmap, name: str) -> ReplaceFileOptions | None:
     # determine the file name length to skip
     name_len = int.from_bytes(f.read(4), "little")
     f.seek(index + name_len)
-    
     offset, size = struct.unpack_from("<QQ", f.read(8 + 8))
     md5_offset = f.tell()
 
     return ReplaceFileOptions(offset, size, md5_offset)
 
+def replace_resource(f: mmap.mmap, new_content: bytes, options: ReplaceFileOptions) -> None:
+    # no idea what the header does, just copy it as is
+    f.seek(options.offset)
+    original_header = f.read(0x20)
+    
+    # TODO header is the same, so why would we write and read it again?
+    content = original_header + new_content
+    if (len(content) > options.size):
+        raise ValueError("Replacement file too big")
+    
+    # just pad with zeroes and hope it works
+    content = content + b"\x00" * (options.size - len(content))
+    f[options.offset:options.offset + options.size] = content
+    
+    # never checked what would happen if hash is not modified
+    hash = bytes.fromhex(hashlib.md5(content).hexdigest())
+    f[options.hash_offset:options.hash_offset + 16] = hash
+
+#endregion
 
 def main(config: TypedConfigNamespace):
 
@@ -86,9 +107,15 @@ def main(config: TypedConfigNamespace):
 
     replacements = (asset for asset in os.listdir(config.replacements_folder) if os.path.isfile(os.path.join(config.replacements_folder, asset)))
     for file in replacements:
+        full_path = os.path.join(config.replacements_folder, file)
         options = try_to_find_resource(f, file)
         if options is not None:
-            print(f"{file}: found at 0x{options.offset:x}, size 0x{options.size:x}, md5 at 0x{options.hash_offset:x}")
+            with open(full_path, "rb") as replacement:
+                content = replacement.read()
+            try:
+                replace_resource(f, content, options)
+            except ValueError:
+                print(f"{file}: replacement too big, unable to can")
         else:
             print(f"{file}: not found in file, skipping.")
 
