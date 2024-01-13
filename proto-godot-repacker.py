e4a0f026a6ebacc6bf17f2873767de9696a69692
Bn4 <bnfour@bnfour.net>
2024-01-13 15:12:12 +0700

    argument parsing probably implemented

--- proto-godot-repacker.py
+++ proto-godot-repacker.py
@@ -1,14 +1,24 @@
 #!/usr/bin/env python3
 
 import argparse
+import os
 
 def get_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description="Allegedly working repacker for Godot resources, in a very blunt way.",
         epilog="Limitations exist, check the docs!")
     
-    # TODO actual arguments for resources file and replacements folder
+    parser.add_argument("resfile", type=argparse.FileType("rb+"), help="Godot resource pack (.pck) or self-contained executable to patch")
+    parser.add_argument("replacements_folder", type=dir_type, help="folder that holds replacement assets")
 
     return parser.parse_args()
+
+def dir_type(path: str) -> str:
+    """Checks that the provided path is a path to existing folder"""
+    path = os.path.realpath(path)
+    if os.path.isdir(path):
+        return path
+    else:
+        raise argparse.ArgumentTypeError(f"{path} is not a valid folder name")
 
 def main(config: argparse.Namespace):
 
